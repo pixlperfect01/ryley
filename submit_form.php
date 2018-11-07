1ba26b3111b2a5e4903077c1217c8e91d8133c67
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-11-06 19:13:07 -0600

    Create submit_form.php

---
+++ submit_form.php
@@ -0,0 +1,19 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "williamsjpoms.6a@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = "Ryley";
+    $last_name = "Jenkins";
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", I'll contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
