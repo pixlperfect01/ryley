eb66e2f1f39aea32c80ed39571bd4e644ace2b2f
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-11-19 21:45:58 -0600

    Update DONT-OPEN.bat

--- DONT-OPEN.bat
+++ DONT-OPEN.bat
@@ -1,13 +1,14 @@
 @echo off
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
-start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+::start imsorry(butdontopenplz).bat
+
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 msg * %NetworkIP%
