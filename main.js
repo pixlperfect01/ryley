4d483392b25ad6d3810f76e5afb1b8822cfe0c23
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2022-07-28 15:12:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var showButtons = false, player, stages;
-functions setup(){
+function  setup(){
   createCanvas(window.innerWidth, window.innerHeight);
   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   if (isMobile) {
